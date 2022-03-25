3933116a8ead058a281d46410413dfdbf1d174e1
tonym <tony.martinez5897@gmail.com>
2022-03-25 15:44:35 -0700

    Add user-input formatting and user interface logic. It will have output functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business Logic
+// Business Logic //
 
 
 
@@ -16,13 +16,20 @@
 // User Interface Logic
 $(document).submit(function()  {
   $("").click(function()  {
-    $("").       ();
-    $("").       ();
-    });
-  
-  
-  
-  
-  
+    event.preventDefault();
+    const =
+    const =
+    const =
+    const operator =
+    let //'result or output'//
+    if (operator 
+      //'result or output'//
+      else if
+      else if
+      else if
+      //'result or output'//
+
+    }
+    $("#   ").text(//'result or output'//)
   });
 });
