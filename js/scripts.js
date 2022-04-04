8dbe4a628172ce25a76b56653c2e8d809a01eb4f
tonym <tony.martinez5897@gmail.com>
2022-04-04 08:49:50 -0700

    add user input forms, it will gave functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
-  $(".oneWay").text(person); {
+  $(".oneWay").text(pickPerson); {
   if (pick1 === "1" && pick3 != "2" || pick1 != "1" && pick3  === "1") {
     $(".swift").show;
     }
     else if(pick1 === "2" || pick3 === "3") {
     } else if (pick1 ==="2" || pick3 === "3") {
-      $(".other").show;
+      $(".javascript").show;
     }
-  //   else if() {
-  //   $("#").show();
-  //   $("#").hide();
-  // }
-  // else if() {
-  // alert("blah")
-  // }
-    else {
-    alert("yahh")
+     else if(pick3 === "2") {
+    } else if (pick3 === "2") {
+      $(".java").show;
+    }
+     else if(pick3 === "2") {
+    } else if (pick3 === "2") {
+      $(".C++").show;
+    }
+    else if(pick3 === "1") {
+    } else if (pick3 === "1") {
+      $(".ruby").show;
+    }
+    else if(pick3 === "1") {
+    } else if (pick3 === "1") {
+      $(".ruby").show;
+    }
+     else {
+     alert("blah")
+     }
+    } else {
+      alert("yahh")
     }
   });  
 });
