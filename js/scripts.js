36f56bdeab292f01f679ef7cc812baf1402fa467
tonym <tony.martinez5897@gmail.com>
2022-04-04 08:09:49 -0700

    adding logic, reorganizing user-input forms.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,29 @@
-// Business Logic //
-function add(number1, number2)  {
-  return number1 + number2;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// User Interface Logic
-$(document).ready(function()  {
-  $("choice#Time").submit(function()  {
-    event.preventDefault();
-    const number1 = parseInt($("#").val());
-    const number2 = parseInt($("#").val());
-    const number3 = parseInt($("#").val());
-    const number4 = parseInt($("#").val());
-    const number5 = parseInt($("#").val());
-    
-    // Let blah blah
-    if (operator 
-      //'result or output'//
-      else if
-      else if
-      else if
-      //'result or output'//
-
+$(document).ready(function() {
+  $(".oneWay").text(person); {
+  if (pick1 === "1" && pick3 != "2" || pick1 != "1" && pick3  === "1") {
+    $(".swift").show;
     }
-    $("#   ").text(//'result or output'//)
-  });
+    else if(pick1 === "2" || pick3 === "3") {
+    } else if (pick1 ==="2" || pick3 === "3") {
+      $(".other").show;
+    }
+  //   else if() {
+  //   $("#").show();
+  //   $("#").hide();
+  // }
+  // else if() {
+  // alert("blah")
+  // }
+    else {
+    alert("yahh")
+    }
+  });  
 });
+
+
+// (event) {
+//   event.preventDefault();
+//   submit(function(event)
+
+// $("#").show();
+// $("#").hide();
