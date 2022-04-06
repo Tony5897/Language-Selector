7e796ad40cde3b47b814d1a9a8ecdd959fdf81cd
tonym <tony.martinez5897@gmail.com>
2022-04-06 08:16:01 -0700

    add output, it will have functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,134 +1,30 @@
 //Business Logic ---------
 
-  
-
-
-
-
 
 //UI Logic --------------
 $(document).ready(function() {
-  $("button").click(function() {
-    let radio = $("input[type=radio][name=pick]").filter(":checked")[0];
-    if (radio === pick1 === "1" && pick3 != "2" || pick1 != "1" && pick3  === "1") {
-    $("#swift").hide();
-        alert("#swift").show();
-    }
+  $("#docForm").submit(function() {
+
+  let pick1 = $("input#pick1").val();
+  let pick2 = $("input#pick2").val();
+  let pick3 = $("input#pick3").val();
+  let pick4 = $("input#pick4").val();
+  let pick5 = $("input#pick5").val();
+  
+  if (pick1 === "1" && pick3 != "2" || pick1 != "1" && pick3  === "1") {
+    $("#output").text("You should learn Swift!").show();
+  }
     else if(pick1 === "2" || pick3 === "3") {
     } else if (pick1 ==="2" || pick3 === "3") {
-      $(".javascript").hide();
+      $("#output").text("You should learn javaScript").show();
     }
     else if(pick3 === "2") {
     } else if (pick3 === "2") {
-      $(".java").hide();
-    }
-    else if(pick2 === "2") {
-    } else if (pick2 === "2") {
-      $(".C++").hide();
-    }
-    else if(pick4 === "2") {
-    } else if (pick4 === "2") {
-      $(".ruby").hide();
-    }
-    else if(pick5 === "1") {
-    } else if (pick5 === "1") {
-      $(".kotlin").hide();
-          alert(value);
+      $("#output").text("You should learn Kotlin").show();
     } 
     else {
-      alert("Nothing is selected");
+      $("#output").text("Nothing is selected").show();
     }
-  })  
+    event.preventDefault();
+});  
 });
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //   // $("#radForm").submit(function(event) {
-
-  //     //     if (value === pick1 === "1" && pick3 != "2" || pick1 != "1" && pick3  === "1") {
-  //     $(".swift").hide();
-  //     }
-  //     else if(pick1 === "2" || pick3 === "3") {
-  //     } else if (pick1 ==="2" || pick3 === "3") {
-  //       $(".javascript").hide();
-  //     }
-  //     else if(pick3 === "2") {
-  //     } else if (pick3 === "2") {
-  //       $(".java").hide();
-  //     }
-  //     else if(pick2 === "2") {
-  //     } else if (pick2 === "2") {
-  //       $(".C++").hide();
-  //     }
-  //     else if(pick4 === "2") {
-  //     } else if (pick4 === "2") {
-  //       $(".ruby").hide();
-  //     }
-  //     else if(pick5 === "1") {
-  //     } else if (pick5 === "1") {
-  //       $(".kotlin").hide();
-  //         alert(value);
-  //     } 
-  //     else {
-  //       alert("Nothing is selected");
-  //     }
-  //   })  
-  // });
-
-
-
-
-
-
-
-
-
-
-
-// <div class="container">
-//     <div class="jumbotron">
-//       <h1>CompCompatibility</h1>
-//       <h6>"You can program a computer, but <br>you can't program what you enjoy doing."</h6>
-//     </div>
-//     <form>
-//       <input id="target" type="text" value="1">
-//     </form>
-//       <select>
-//         <option </option>
-//       </select>
-//     </form>
- 
-  
