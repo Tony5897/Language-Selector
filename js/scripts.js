41833cefb7a1eaeb58489b80a39b8ec7598c2be7
tonym <tony.martinez5897@gmail.com>
2022-04-07 08:14:31 -0700

    add new forms, fixing output bug.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,30 @@
-//Business Logic ---------
-
-
-//UI Logic --------------
 $(document).ready(function() {
-  $("#docForm").submit(function() {
+  $("doc.Form").submit(function() {
 
-  let pick1 = $("input#pick1").val();
-  let pick2 = $("input#pick2").val();
-  let pick3 = $("input#pick3").val();
-  let pick4 = $("input#pick4").val();
-  let pick5 = $("input#pick5").val();
-  
-  if (pick1 === "1" && pick3 != "2" || pick1 != "1" && pick3  === "1") {
-    $("#output").text("You should learn Swift!").show();
-  }
-    else if(pick1 === "2" || pick3 === "3") {
-    } else if (pick1 ==="2" || pick3 === "3") {
-      $("#output").text("You should learn javaScript").show();
-    }
-    else if(pick3 === "2") {
-    } else if (pick3 === "2") {
-      $("#output").text("You should learn Kotlin").show();
+    let queOne = $("select#queOne").val();
+    let queTwo = $("select#queTwo").val();
+    let queThree = $("select#queThree").val();
+    // let pick4 = $("input#pick4").val();
+    // let pick5 = $("input#pick5").val();
+
+    if (queOne === "cat" && queTwo === "coffee" && queThree === "apple") {
+      $("#output").text("You should learn Swift").show();
+    } else if(queTwo === "tea") {
+      $("#output").text("You shoud learn kotlin.").show();
+  // } else if(pick1 === "2" || pick3 === "3") {
+  //   } else if (pick1 ==="2" || pick3 === "3") {
+  //     $("#output").text("You should learn javaScript").show();
+  //   }
+  //   else if(pick3 === "2") {
+  //   } else if (pick3 === "2") {
+  //     $("#output").text("You should learn Kotlin").show();
     } 
     else {
       $("#output").text("Nothing is selected").show();
     }
     event.preventDefault();
-});  
+  });  
 });
+
+
+//Swift, Kotlin, C#,
